ec2ed59556ac909acda723d0ccc8114a35c8b2c5
Almujim Hadjili <almujim.hadjili@gmail.com>
2025-10-21 21:56:18 +0800

    style: Update career quiz database schema by adding core_subject_tb and modifying quiz_results_tb structure

--- Config/Database/career_quiz_db.sql
+++ Config/Database/career_quiz_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2025 at 05:22 PM
+-- Generation Time: Oct 21, 2025 at 03:55 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `core_subject_tb`
+--
+
+CREATE TABLE `core_subject_tb` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `Statistics_and_Probability` varchar(255) NOT NULL,
+  `Physical_Science` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz_results_tb`
+--
+
+CREATE TABLE `quiz_results_tb` (
+  `result_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `session_id` varchar(255) DEFAULT NULL,
+  `quiz_data` longtext NOT NULL,
+  `recommended_careers` longtext NOT NULL,
+  `completion_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `is_guest` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users_tb`
 --
 
@@ -35,8 +64,28 @@ CREATE TABLE `users_tb` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `users_tb`
+--
+
+INSERT INTO `users_tb` (`user_id`, `userName`, `userEmail`, `userPassword`) VALUES
+(1, 'Jim Hadjili', 'jim.hadjili@gmail.com', '$2y$10$qQG1S4NJnbRSGEfcOBaa7.a.gXyVdAMjl/C8VlwqC6dA/Mzyx2uGG');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `core_subject_tb`
+--
+ALTER TABLE `core_subject_tb`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `quiz_results_tb`
+--
+ALTER TABLE `quiz_results_tb`
+  ADD PRIMARY KEY (`result_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users_tb`
@@ -49,10 +98,32 @@ ALTER TABLE `users_tb`
 --
 
 --
+-- AUTO_INCREMENT for table `core_subject_tb`
+--
+ALTER TABLE `core_subject_tb`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `quiz_results_tb`
+--
+ALTER TABLE `quiz_results_tb`
+  MODIFY `result_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users_tb`
 --
 ALTER TABLE `users_tb`
-  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `quiz_results_tb`
+--
+ALTER TABLE `quiz_results_tb`
+  ADD CONSTRAINT `quiz_results_tb_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users_tb` (`user_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

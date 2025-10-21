d4ce15f0368865a238ef3a7b61db68e567f4cc54
Almujim Hadjili <almujim.hadjili@gmail.com>
2025-10-21 23:21:26 +0800

    style: Add quizPage.Css for improved styling and update link reference in quizPageHeadtag.php

--- Content/Includes/quizPageComponents/quizPageHeadtag.php
+++ Content/Includes/quizPageComponents/quizPageHeadtag.php
@@ -5,7 +5,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <script src="../../Assets/Scripts/tailwindConfig.js"></script>
-    <link rel="stylesheet" href="../Includes/quizPageComponents/quizPage.css">
+    <link rel="stylesheet" href="../../Assets/Styles/quizPage.Css">
     <link rel="icon" type="image/png" href="../../Assets/Images/logo.png">
     <title>CareerPath Quiz - <?php echo $is_registered ? 'Registered User' : 'Guest Mode'; ?></title>
 </head>

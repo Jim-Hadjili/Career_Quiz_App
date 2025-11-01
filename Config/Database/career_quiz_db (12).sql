88066b22aa8c189e028573c2d5c144c3a6fa1265
Almujim Hadjili <almujim.hadjili@gmail.com>
2025-11-01 22:22:20 +0800

    feat: Add initial database schema and structure for career quiz application

---
+++ Config/Database/career_quiz_db (12).sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 01, 2025 at 03:21 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `career_quiz_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `core_subject_tb`
+--
+
+CREATE TABLE `core_subject_tb` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `Statistics_and_Probability` varchar(255) NOT NULL,
+  `Physical_Science` varchar(255) NOT NULL,
+  `oral_comm_context` varchar(255) NOT NULL,
+  `general_math` varchar(255) NOT NULL,
+  `earth_life_sci` varchar(255) NOT NULL,
+  `ucsp` varchar(255) NOT NULL,
+  `reading_writing` varchar(255) NOT NULL,
+  `lit21_ph_world` varchar(255) NOT NULL,
+  `media_info_lit` varchar(255) NOT NULL,
+  `mbti_type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz_results_tb`
+--
+
+CREATE TABLE `quiz_results_tb` (
+  `result_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `session_id` varchar(255) DEFAULT NULL,
+  `quiz_data` longtext NOT NULL,
+  `recommended_careers` longtext NOT NULL,
+  `completion_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `is_guest` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `selected_career_tb`
+--
+
+CREATE TABLE `selected_career_tb` (
+  `selectedCareer_id` int(11) NOT NULL,
+  `result_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `career_selected` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_tb`
+--
+
+CREATE TABLE `users_tb` (
+  `user_id` int(255) NOT NULL,
+  `userName` varchar(255) NOT NULL,
+  `userEmail` varchar(255) NOT NULL,
+  `userRole` varchar(255) DEFAULT NULL,
+  `userPassword` varchar(255) NOT NULL,
+  `registration_date` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users_tb`
+--
+
+INSERT INTO `users_tb` (`user_id`, `userName`, `userEmail`, `userRole`, `userPassword`, `registration_date`) VALUES
+(1, 'Admin', 'admin@gmail.com', 'Admin', '$2y$10$bPwcxJ7Uzm1rBx3TMhaAye.yR2V7CykSkHzZRLfwcH/hbpFeXisDC', '2025-11-01 21:58:31');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `core_subject_tb`
+--
+ALTER TABLE `core_subject_tb`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `quiz_results_tb`
+--
+ALTER TABLE `quiz_results_tb`
+  ADD PRIMARY KEY (`result_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `selected_career_tb`
+--
+ALTER TABLE `selected_career_tb`
+  ADD PRIMARY KEY (`selectedCareer_id`);
+
+--
+-- Indexes for table `users_tb`
+--
+ALTER TABLE `users_tb`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `core_subject_tb`
+--
+ALTER TABLE `core_subject_tb`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `quiz_results_tb`
+--
+ALTER TABLE `quiz_results_tb`
+  MODIFY `result_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `selected_career_tb`
+--
+ALTER TABLE `selected_career_tb`
+  MODIFY `selectedCareer_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users_tb`
+--
+ALTER TABLE `users_tb`
+  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `quiz_results_tb`
+--
+ALTER TABLE `quiz_results_tb`
+  ADD CONSTRAINT `quiz_results_tb_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users_tb` (`user_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
